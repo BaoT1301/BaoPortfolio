e9a1d4df50e56e6fef0cde25fe3d595a3e95590b
Bao Tran <baot130105@gmail.com>
2025-12-30 22:09:45 -0500

    Initial commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,10 @@
+export const LINKS = {
+  github: "https://github.com/BaoT1301",
+  linkedin: "https://www.linkedin.com/in/baot1301",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
